0dbd11543c1b4818523b43c9f0a43ac2a867bb47
Ming-Hsuan Tao <mhtao@umich.edu>
2020-04-28 15:18:18 -0400

    final

--- final_project.py
+++ final_project.py
@@ -9,22 +9,19 @@ class City:
     '''a city, description text and attractions
     Instance Attributes
     -------------------
-    text: string
-        description of the city 
     city: string
         city and state/country of the attraction (e.g. 'San Francisco, California or Taipei, Taiwan')
+    text: string
+        description of the city 
     attraction: 
         the city's top 5 attractions and details
     '''
-
     def __init__(self ,city = None, text = None,attraction = None):
         
         self.city = city #one
         self.text = text #one 
         self.attraction = attraction #one list of the five attractions
     
-    # San Francisco: Every neighborhood in San Francisco has its own personality, 
-    # from the hippie chic of the Upper Haight to the hipster grit of the Mission...
     def about(self):
         return self.city + ": " + self.text
 
@@ -32,10 +29,10 @@ class Attraction: #consruct first and add to list. then construct the city.
     '''a city's top 5 attractions and details
     Instance Attributes
     -------------------
-    name: string
-        the attraction ranking and name of atraction(e.g. '1. Alcatraz Island')
     category: string
         the category of an attraction (e.g. 'Sights & Landmarks')
+    name: string
+        the attraction ranking and name of atraction(e.g. '1. Alcatraz Island')
     rating: string
         the average rating of the attraction (e.g. '4.5')
     review: string
@@ -46,38 +43,10 @@ class Attraction: #consruct first and add to list. then construct the city.
         self.name = name #five
         self.rating = rating #five
         self.review = review #five
-        #self.ranking = ranking #five
     
     #1. Alcatraz Island (Sights & Landmarks): 4.5 stars, 55.171 reviews
     def info(self):
         return self.name + " (" + self.category + "): " + str(self.rating)+ " stars, " + self.review 
-
-#part 2
-"""def get_city_text_instance(site_url,attraction):
-    '''Make an instances from a city information url.
-    
-    Parameters
-    ----------
-    site_url: string
-        The URL for a city information url in trip advisor.
-    
-    Returns
-    -------
-    instance
-        a list of five attraction instances
-    '''
-    #BASE_URL = site_url 
-    #resp = make_url_request_using_cache(site_url)
-    #soup = BeautifulSoup(resp,'html.parser')
-    
-    city1  = soup.find(class_ = "social-sections-GeoOverviewSection__header--zogLR") #one one class
-    city = city1.find(class_="ui_header h2").text.strip() #only one under city #prints "Ann Arbor, Michigan" == $0 <!-- -->
-    text = soup.find(class_ = "cPQsENeY").text.strip()  #only one class
-
-    print(city)
-    print(text)
-    city_info= City(city, text,attraction)
-    return city_info"""
 
 
 def get_instances_for_attraction(attraction_url):
@@ -114,7 +83,11 @@ def get_instances_for_attraction(attraction_url):
             rating = int(rating1.find("span")["class"][1][-2:])/10 
             #print(f"rating {rating}")
             #rating = attraction.find(class_= "ui_bubble_rating")['class'][1] #for value in element["class"] #class_= "ui_bubble_rating bubble_45"
-            review = attraction.find(class_ ="reviewCount styleguide-bubble-rating-BubbleRatingWithReviewCount__reviewCount--37tMc").text.strip()
+            review1 = attraction.find(class_ ="reviewCount styleguide-bubble-rating-BubbleRatingWithReviewCount__reviewCount--37tMc").text.strip()
+            review2 = (review1.split(" ")[0])
+            review = int(review2.replace(',', ''))
+            #rint(preview)
+            #print(type(review))            
             #print(f"review {review}")
             attraction = Attraction(category,name, rating,review) #creating attraction instance, make sure order is correct
             #print(f"attraction{attraction}")
@@ -124,6 +97,7 @@ def get_instances_for_attraction(attraction_url):
             break
     #print(f"list{lists}")
     return lists # five attraction instances all in a list
+
 
 #make and save cache
 def open_cache():
@@ -148,6 +122,7 @@ def open_cache():
         cache_dict = {}
     return cache_dict
 
+
 def save_cache(cache_dict):
     ''' Saves the current state of the cache to disk
     
@@ -165,16 +140,10 @@ def save_cache(cache_dict):
     fw.write(dumped_json_cache)
     fw.close() 
 
-def city_dict(list):
 
-#1. search for city 
+def city_dict(list): 
     BASEURL = "https://www.tripadvisor.com"
     city_dict={}
-    #if bool(city_dict):
-    #    pass
-    #    print("there is a dictionary")
-    #else:
-    #    city_dict ={}
     for url in list:
         popular_cities = requests.get(url).text #json file
         soup = BeautifulSoup(popular_cities, 'html.parser')
@@ -185,14 +154,9 @@ def city_dict(list):
         for pop_cities in city_sites:
             #print(pop_cities)
             city_dict[pop_cities.text.strip()]= BASEURL+pop_cities.find("a")["href"]
-    #get_link = search_results.find(class_="ui_columns is-mobile result-content-columns") 
-    #city_link = get_link["onclick"].split(",")[3]
-    #city_info = requests.get(BASEURL + city_link) 
-    #params = {"q": city} #city name (e.g: San Francisco) #what is my key? my value is San Francisco.
-    #unique_key = construct_unique_key(BASEURL, params) #https://www.tripadvisor.com/Search?q=san%20francisco&searchSessionId=8F700871742D384F933251EC07147FFE1586551116577ssid&sid=E241DDA62B22634BF3C7848A0DFC22DA1586551117396&blockRedirect=true&rf=3
-    #search = requests.get(BASEURL, params) #json file
     print(city_dict)
     return city_dict
+
 
 def open_links(city_info_url):
 
@@ -322,12 +286,6 @@ def make_url_request_using_cache(url,params={}):
             result ={}
     return result #text
 
-    #lst[:4] For ele in lst: ele.find(div=, class_=) 
-
-
-    # create tables and columns
-    # city table: City and text, state
-    # attraction table: ID(autoincrement), attraction name, rating, reviews, category, City (foreign key)
 
 def create_db():
     conn = sqlite3.connect('trip_advisor.sqlite')
@@ -347,8 +305,8 @@ def create_db():
         CREATE TABLE 'Attraction'(
             'AttractionName' TEXT PRIMARY KEY UNIQUE,
             'Category' TEXT NOT NULL,
-            'Rating' FLOAT NOT NULL,
-            'Review' TEXT NOT NULL,
+            'Rating' REAL NOT NULL,
+            'Review' INTEGER NOT NULL,
             'CityName' TEXT NOT NULL
         )
     '''
@@ -359,6 +317,7 @@ def create_db():
     conn.commit()
     conn.close()
 
+
 def load_city(cities):#cities are city class instances
     conn = sqlite3.connect('trip_advisor.sqlite')
     cur = conn.cursor()
@@ -368,6 +327,7 @@ def load_city(cities):#cities are city class instances
     for city_info in cities: #objects from the city class
         cur.execute(insert_city, [city_info.city, city_info.text])
     conn.commit()
+
 
 def load_attraction(cities):
     conn = sqlite3.connect('trip_advisor.sqlite')
@@ -386,29 +346,13 @@ def load_attraction(cities):
             cur.execute(insert_attraction, [name, category, rating, review, city_name])
     conn.commit()
 
-
-
-#create_db()
-
-    # put all the data in sql:  
-    # select (*) from city where city = "Ann Arbor". If no result, info is not in database. Call request from code. 
-    # write another function about Inserting. Call the function inside of the exisitng functions. 
-    # write info into database before writing class. 
-
-#city_dict = {"Paris":"https://www.tripadvisor.com/Tourism-g187147-Paris_Ile_de_France-Vacations.html", "Ann Arbor": 
-#"https://www.tripadvisor.com/Tourism-g29556-Ann_Arbor_Michigan-Vacations.html", 
-#"New York":"https://www.tripadvisor.com/Tourism-g60763-New_York_City_New_York-Vacations.html"}
-
-
 if __name__ == "__main__":
     create_db()
     city_dict = city_dict(["https://www.tripadvisor.com/TravelersChoice-Destinations-cPopular-g191","https://www.tripadvisor.com/TravelersChoice-Destinations-cPopular-g1"])
-    #city_dictionary = city_dict("https://www.tripadvisor.com/TravelersChoice-Destinations-cPopular-g191")
     print(city_dict.values())
     cities = []
     for url in city_dict.values():
-        city_instances = open_links(url)
-   #download data from multiple cities
-        cities.append(city_instances)
+        city_instances = open_links(url) #download data from multiple cities
+        cities.append(city_instances) 
     load_city(cities)
     load_attraction(cities)
--- presentation.py
+++ presentation.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, render_template
+ 
+from flask import Flask, render_template, request
 import random
 import requests
 import sqlite3
@@ -7,17 +7,54 @@ import plotly.graph_objs as go
 
 app = Flask(__name__)
 
-@app.route('/plot')
-def plot():
+@app.route('/')
+def index():
     conn = sqlite3.connect('trip_advisor.sqlite')
     cur = conn.cursor()
-    cur.execute('Select CityName, avg(rating), avg(review) from Attraction GROUP BY CityName')
+    cur.execute('Select CityName from Attraction GROUP BY CityName')
+    results = cur.fetchall()
+    length = len(results)
+
+    #print(results)
+    conn.close()
+    return render_template('index.html',results= results, length=length)
+
+@app.route('/text', methods=['POST'])
+def handle_the_form():  
+    conn = sqlite3.connect('trip_advisor.sqlite')
+    cur = conn.cursor()
+    city = request.form["cities"]
+    cur.execute(f"SELECT Text from City WHERE CityName = '{city}' ")
+    text_result = cur.fetchall()[0][0]
+    
+    print(text_result)
+    conn.close()
+    return render_template('text.html',city = city, text = text_result)
+
+@app.route('/attraction/<city>')
+def attraction(city):
+    conn = sqlite3.connect('trip_advisor.sqlite')
+    cur = conn.cursor()
+    print(city)
+    cur.execute(f"Select AttractionName, Category, Rating, Review, CityName from Attraction WHERE CityName = '{city}'")
+    results = cur.fetchall() #a list of five attractions
+
+    #print(results)
+    conn.close()
+    return render_template('attraction.html',city = city, results= results)
+
+
+@app.route('/plot_avg_rating')
+def plot_rating():
+    conn = sqlite3.connect('trip_advisor.sqlite')
+    cur = conn.cursor()
+    cur.execute('Select CityName, avg(rating) from Attraction GROUP BY CityName Order by avg(rating)')
     results = cur.fetchall()
     conn.close()
 
     x_vals = []
     y_vals = []
-    for (cityname, avg_rating, avg_review) in results:
+    for (cityname, avg_rating) in results:
         x_vals.append(cityname)
         y_vals.append(avg_rating)
     bars_data = go.Bar(
@@ -26,7 +63,30 @@ def plot():
     )
     fig = go.Figure(data=bars_data)
     div = fig.to_html(full_html=False)
-    return render_template("plot.html", plot_div=div)
+    return render_template("plot_avg_rating.html", plot_div=div)
+
+@app.route('/plot_avg_reviews')
+def plot_review():
+    conn = sqlite3.connect('trip_advisor.sqlite')
+    cur = conn.cursor()
+    cur.execute('Select CityName, avg(review) from Attraction GROUP BY CityName Order by avg(review)')
+    results = cur.fetchall()
+    conn.close()
+
+    x_vals = []
+    y_vals = []
+    for (cityname, avg_review) in results:
+        #print(cityname, avg_review)
+        x_vals.append(cityname)
+        y_vals.append(avg_review)
+    bars_data = go.Bar(
+        x=x_vals,
+        y=y_vals
+    )
+    fig = go.Figure(data=bars_data)
+    div = fig.to_html(full_html=False)
+    return render_template("plot_avg_reviews.html", plot_div=div)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
